bdbfb74557fbfe0a65d2adf647aea70040aa6059
M3hTi <mehdinasir502@yahoo.com>
2025-01-18 13:24:06 +0330

    feat: implement search functionality in Search component(callBack Handler)

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,32 @@ import './App.css'
 
 function App() {
   console.log('App  renders');
+
+  function handleSearch(term){
+    console.log(`search input transform to parent:${term}`);
+  }
   return (
     <>
-      <Search />
+      <Search onSearch={handleSearch} />
     </>
   )
 }
 
-function Search() {
+function Search(props) {
   console.log('search renders');
+  console.log(props);
 
   const [searchTerm , setSearchTerm] = React.useState('')
 
-
+  function handleInput(e) {
+    const newValue = e.target.value;
+    setSearchTerm(newValue)
+    props.onSearch(newValue)  // Pass the new value directly instead of searchTerm
+  }
   return(
     <>
       <label htmlFor="srch">Search:</label>
-      <input type="text" id='srch' onInput={(e) => setSearchTerm(e.target.value)}/>
+      <input type="text" id='srch' onInput={handleInput}/>
 
       <p>
         Searching for <strong>{searchTerm}</strong>.
